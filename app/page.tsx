7d2da8cd22116c14f3317a7ce122174fa1137519
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2025-06-14 13:36:05 +0300

    made a home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,56 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link"
+
 export default function Home() {
-  <></>
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-[calc(100vh-52px)] overflow-hidden p-4">
+      <div className="flex flex-col items-center space-y-4 mt-8">
+        <Button
+          className="
+            w-[264px] h-[44px]
+            flex items-center justify-center
+            text-xs leading-[12px] font-medium
+            text-gray-900 bg-white
+            rounded-full shadow-sm
+            hover:bg-gray-100
+          "
+        >
+          Registration
+        </Button>
+        
+        <Button
+          className="
+            w-[264px] h-[44px]
+            flex items-center justify-center
+            text-xs leading-[12px] font-medium
+            text-gray-900 bg-white
+            rounded-full shadow-sm
+            hover:bg-gray-100
+          "
+        >
+          Login
+        </Button>
+
+        <div className="pt-6">
+          <Link
+            href="/orders"
+            className="
+              w-[296px] h-[36px]
+              flex items-center justify-center
+              text-xs leading-[12px] font-medium
+              text-gray-900 bg-white
+              rounded-full shadow-sm
+              hover:bg-gray-100
+              border border-gray-300
+            "
+          >
+            <h3 className="font-bold">Orders</h3>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
