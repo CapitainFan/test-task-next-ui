44ffd0650bb0fdd6bb0147139e2bd7fbf94cd12b
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2025-06-19 15:33:26 +0300

    git add .

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Другие настройки Next.js
+  reactStrictMode: true,
+  swcMinify: true,
+};
